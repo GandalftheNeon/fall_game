276197d4234c2ce9069ce45a1bda29589c7956f5
mfro <maxwell.froehlich@gmail.com>
2018-10-13 00:04:17 -0400

    LeafMotion

---
+++ what_goes_up/Assets/Scripts/LeafMotion.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Leaf : MonoBehaviour
+{
+    Vector2 position;
+    Vector2 velocity;
+
+    // Use this for initialization
+    void Start()
+    {
+        position = new Vector2();
+        velocity = new Vector2(24, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var rot = velocity.magnitude * 0.002f;
+        if (velocity.x > 0)
+            rot = -rot;
+
+        velocity = velocity.Rotate(rot);
+
+        if (velocity.y > 0)
+            velocity += velocity.normalized * -1;
+        else
+            velocity += velocity.normalized * 1;
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+            velocity = velocity.Rotate(-0.02f);
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+            velocity = velocity.Rotate(0.02f);
+    }
+}
---
+++ what_goes_up/VecExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public static class VecExtensions
+{
+    public static Vector2 Rotate(this Vector2 vec, float angle)
+    {
+        var sin = Mathf.Sin(angle);
+        var cos = Mathf.Cos(angle);
+
+        var x = (cos * vec.x) - (sin * vec.y);
+        var y = (sin * vec.x) + (cos * vec.y);
+
+        return vec;
+    }
+}
